348385b9c898c714ab55f5599fe34effff940c65
adamTab <tabievadam@gmail.com>
2022-11-15 13:54:00 +0100

    Initial Commit

---
+++ ProgectForGitAndBranch/ProgectForGitAndBranchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgectForGitAndBranchApp.swift
+//  ProgectForGitAndBranch
+//
+//  Created by Адам Табиев on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProgectForGitAndBranchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
